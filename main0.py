c1ae2b4c2759c1e1aa3117eabb4095a4ab2f775e
PavelKislyaev <pavel.kislyayev@mail.ru>
2024-08-05 18:19:12 +0300

    Add files via upload

--- main0.py
+++ main0.py
@@ -1,6 +1,6 @@
 number_of_completed_homework_assignments = 12
 number_of_hours_spent = 1.5
 course_name = ("Python")
-time_for_one_task = 1.5/12
-print("Курс:",course_name,",","всего задач:",number_of_completed_homework_assignments, "," ,"затрачено часов:", number_of_hours_spent,"," ,"среднее выполнение", time_for_one_task)
+time_for_one_task = 12/1.5
+print("Курс:",course_name,",","всего задач:",number_of_completed_homework_assignments, "," ,"затрачено часов:", number_of_hours_spent,"," ,"среднее выполнение", time_for_one_task,"часов")
 
